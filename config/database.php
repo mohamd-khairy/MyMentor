1f0891e8f031b9d678c61beb943857004df9435e
mohamd-khairy <mohamed.khairy.eg@gmail.com>
2020-02-20 11:22:45 +0200

    added postgres database configuration2

--- config/database.php
+++ config/database.php
@@ -84,10 +84,11 @@ return [
             'database' => ltrim($DATABASE_URL["path"], "/"),
             'username' => $DATABASE_URL["user"],
             'password' => $DATABASE_URL["pass"],
-            'charset' => 'utf8mb4',
+            'charset' => 'utf8',
             'prefix' => '',
+            'prefix_indexes' => true,
             'schema' => 'public',
-            'sslmode' => 'require',
+            'sslmode' => 'prefer',
         ],
 
         'sqlsrv' => [
