030fbeef8290be76573b206e17dbf6907daac86e
mohamd-khairy <mohamed.khairy.eg@mail.com>
2020-03-16 21:44:22 +0200

    changes

--- config/app.php
+++ config/app.php
@@ -2,7 +2,7 @@
 
 return [
 
-    'host_url' => env('HOST_URL', 'https://infinite-atoll-41509.herokuapp.com/storage'),
+    'host_url' => env('HOST_URL', 'https://infinite-atoll-41509.herokuapp.com'),
     /*
     |--------------------------------------------------------------------------
     | Application Name
