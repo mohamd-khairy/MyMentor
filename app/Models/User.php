230d7218cd7fb0693875fa48a621559fd4cf9634
mohamd-khairy <mohamed.khairy.eg@mail.com>
2020-03-16 22:46:54 +0200

    changes

--- app/Models/User.php
+++ app/Models/User.php
@@ -69,7 +69,7 @@ class User extends Authenticatable implements JWTSubject
 
     public function topics()
     {
-        return $this->hasMany(Topics::class , 'user_id')->limit(1);
+        return $this->hasMany(Topics::class , 'user_id')->first();
     }
 
     /**
