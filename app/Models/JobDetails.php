76d36a5a22e6e01a199e80445ea51278cf8c41af
mohamd-khairy <mohamed.khairy.eg@mail.com>
2020-03-21 14:44:34 +0200

    changes

--- app/Models/JobDetails.php
+++ app/Models/JobDetails.php
@@ -9,11 +9,11 @@ use Illuminate\Support\Collection;
 class JobDetails extends Model
 {
     protected $fillable = ['available_days' , 'available_langs' , 'session_price' ,
-        'session_duration' , 'user_id'];
+        'session_duration' , 'user_id' , 'topic_id'];
 
     protected $hidden = ['created_at' , 'updated_at'];
 
-    protected $with = ['user'];
+    protected $with = ['user','topic'];
     
 
     public function setAvailableLangsAttribute($input)
@@ -75,5 +75,10 @@ class JobDetails extends Model
     public function user()
     {
         return $this->belongsTo(User::class , 'user_id');
+    }
+
+    public function topic()
+    {
+        return $this->belongsTo(Topics::class , 'topic_id');
     }
 }
--- database/migrations/2020_02_23_132715_add_topic_id_to_job_details_table.php
+++ database/migrations/2020_02_23_132715_add_topic_id_to_job_details_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddTopicIdToSessionsTable extends Migration
+class AddTopicIdToJobDetailsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,8 +13,8 @@ class AddTopicIdToSessionsTable extends Migration
      */
     public function up()
     {
-        Schema::table('sessions', function (Blueprint $table) {
-            $table->unsignedBigInteger('topic_id')->after('user_recieve_id')->nullable();
+        Schema::table('job_details', function (Blueprint $table) {
+            $table->unsignedBigInteger('topic_id')->after('user_id')->nullable();
 
             $table->foreign('topic_id')->references('id')
             ->on('topics')->onUpdate('cascade')->onDelete('cascade');
@@ -29,7 +29,7 @@ class AddTopicIdToSessionsTable extends Migration
      */
     public function down()
     {
-        Schema::table('sessions', function (Blueprint $table) {
+        Schema::table('job_details', function (Blueprint $table) {
             $table->dropColumn('topic_id');
         });
     }
