eb5e1f63e0a0c4f1f65e8336cfe6f6c83c586dd1
mohamd-khairy <mohamed.khairy.eg@mail.com>
2020-03-21 16:01:42 +0200

    changes

--- app/Models/JobDetails.php
+++ app/Models/JobDetails.php
@@ -13,9 +13,9 @@ class JobDetails extends Model
 
     protected $hidden = ['created_at' , 'updated_at'];
 
-    protected $with = ['user','topic'];
+    protected $with = ['user'];
     
-    protected $appends = ['photo'];
+    protected $appends = ['photo' , 'topic'];
 
     public function setAvailableLangsAttribute($input)
     {
@@ -66,6 +66,17 @@ class JobDetails extends Model
     {
 
         return $this->user->profile->photo ?? null;
+
+    }
+
+    public function getTopicAttribute()
+    {
+
+        return [
+            'topic' => $this->topic()->topic,
+            'subject' => $this->topic()->subject,
+            'details' => $this->topic()->details,
+        ];
 
     }
 
