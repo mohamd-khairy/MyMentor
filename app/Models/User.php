10c21db6abca0c17017d7c0e6e24706c5b314692
mohamd-khairy <mohamed.khairy.eg@mail.com>
2020-04-25 16:36:19 +0200

    add voyager

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,8 +7,9 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Support\Facades\Hash;
 use Tymon\JWTAuth\Contracts\JWTSubject;
+use TCG\Voyager\Models\User as VUSER;
 
-class User extends Authenticatable implements JWTSubject
+class User extends VUSER implements JWTSubject
 {
     use Notifiable;
 
