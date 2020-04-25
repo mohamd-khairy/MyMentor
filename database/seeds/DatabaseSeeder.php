6596bc00a4a1d6fc5d5bbbd0e83d39e7c0ed16b3
mohamd-khairy <mohamed.khairy.eg@mail.com>
2020-04-25 16:48:44 +0200

    add voyager

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -27,8 +27,7 @@ class DatabaseSeeder extends Seeder
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
             'verified' => 1,
-            'user_type_id' => 1,
-            'role_id' => 1
+            'user_type_id' => 1
         ])->each(function($u){
             $u->profile()->save(factory('App\Models\Profile')->create());
             $u->topicss()->save(factory('App\Models\Topics')->create());
