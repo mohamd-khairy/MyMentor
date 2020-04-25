4ce5db6fafb5db38487b244841ba8327f5170005
mohamd-khairy <mohamed.khairy.eg@mail.com>
2020-04-25 16:47:14 +0200

    add voyager

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -20,36 +20,36 @@ class DatabaseSeeder extends Seeder
         $this->call(LanguagesSeeder::class);
         $this->call(CategoriesSeeder::class);
         
-        // factory('App\Models\User')->create([
-        //     'name' => "mohamed",
-        //     'email' => "m.khairy@evntoo.com",
-        //     'email_verified_at' => now(),
-        //     'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-        //     'remember_token' => Str::random(10),
-        //     'verified' => 1,
-        //     'user_type_id' => 1,
-        //     'role_id' => 1
-        // ])->each(function($u){
-        //     $u->profile()->save(factory('App\Models\Profile')->create());
-        //     $u->topicss()->save(factory('App\Models\Topics')->create());
-        // });
+        factory('App\Models\User')->create([
+            'name' => "mohamed",
+            'email' => "m.khairy@evntoo.com",
+            'email_verified_at' => now(),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'remember_token' => Str::random(10),
+            'verified' => 1,
+            'user_type_id' => 1,
+            'role_id' => 1
+        ])->each(function($u){
+            $u->profile()->save(factory('App\Models\Profile')->create());
+            $u->topicss()->save(factory('App\Models\Topics')->create());
+        });
         
-        // //users
-        // factory('App\Models\User', 10)->create([
-        //     'user_type_id' => 2,
-        // ])
-        // ->each(function($u){
-        //     $u->profile()->save(factory('App\Models\Profile')->create());
-        // });
+        //users
+        factory('App\Models\User', 10)->create([
+            'user_type_id' => 2,
+        ])
+        ->each(function($u){
+            $u->profile()->save(factory('App\Models\Profile')->create());
+        });
 
-        // //mentors
-        // factory('App\Models\User', 10)->create([
-        //     'user_type_id' => 1,
-        // ])
-        // ->each(function($u){
-        //     $u->profile()->save(factory('App\Models\Profile')->create());
-        //     $u->topicss()->save(factory('App\Models\Topics')->create());
-        // });
+        //mentors
+        factory('App\Models\User', 10)->create([
+            'user_type_id' => 1,
+        ])
+        ->each(function($u){
+            $u->profile()->save(factory('App\Models\Profile')->create());
+            $u->topicss()->save(factory('App\Models\Topics')->create());
+        });
 
         
     }
