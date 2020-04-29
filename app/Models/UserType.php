a4729e99e3b282b413c30cdf478c3167817614a2
mohamd-khairy <mohamed.khairy.eg@mail.com>
2020-04-29 02:14:54 +0200

    lang

--- app/Models/UserType.php
+++ app/Models/UserType.php
@@ -3,9 +3,13 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use TCG\Voyager\Traits\Translatable;
 
 class UserType extends Model
 {
+    use Translatable;
+    protected $translatable = ['user_type_name'];
+
     protected $fillable = ['user_type_name'];
 
     protected $hidden = ['created_at' , 'updated_at'];
