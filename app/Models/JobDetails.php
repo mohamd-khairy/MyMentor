532096edb63fd55aa7ac4b9b1da54b09095be24f
mohamd-khairy <mohamed.khairy.eg@mail.com>
2020-05-05 17:31:21 +0200

    fix

--- app/Models/JobDetails.php
+++ app/Models/JobDetails.php
@@ -8,7 +8,7 @@ use Illuminate\Support\Collection;
 
 class JobDetails extends Model
 {
-    protected $fillable = ['available_days' , 'available_langs' , 'session_price' ,
+    protected $fillable = ['available_days' , 'available_langs' , 'session_price' , 'session_duration_type',
         'session_duration' , 'user_id' , 'topic_id'];
 
     protected $hidden = ['created_at' , 'updated_at'];
