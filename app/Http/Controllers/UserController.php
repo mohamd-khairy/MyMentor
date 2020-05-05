0babc1f62e15ca45fd806a231237b3dd294017e9
mohamd-khairy <mohamed.khairy.eg@mail.com>
2020-05-05 18:39:08 +0200

    fix

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -28,7 +28,7 @@ class UserController extends Controller
 
     public function most_popular_mentor()
     {
-      $data = User::with('profile','topics','skills' , 'experienceDetail' , 'job')->where([ 'complete_profile_rate' => 90,'user_type_id' => UserType::where('user_type_name' , 'mentor')->first()->id])->orderBy('complete_profile_rate' , 'desc')->take(5)->get() ?? [];
+      $data = User::with('profile','topics','skills' , 'experienceDetail' , 'job')->where( 'complete_profile_rate' ,'>=', 90)->where(['user_type_id' => UserType::where('user_type_name' , 'mentor')->first()->id])->orderBy('complete_profile_rate' , 'desc')->take(5)->get() ?? [];
 
       return responseSuccess($data , 'data returned successfully');
     }
