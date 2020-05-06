0598583b4fa38c531bac6b3efde080e0b69372c7
mohamd-khairy <mohamed.khairy.eg@mail.com>
2020-05-06 21:51:40 +0200

    init

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -14,8 +14,7 @@ class Kernel extends HttpKernel
      * @var array
      */
     protected $middleware = [
-        \Fruitcake\Cors\HandleCors::class,
-        // \App\Http\Middleware\CORSMiddleware::class,
+        \App\Http\Middleware\CORSMiddleware::class,
         \App\Http\Middleware\TrustProxies::class,
         \App\Http\Middleware\CheckForMaintenanceMode::class,
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
@@ -40,7 +39,7 @@ class Kernel extends HttpKernel
         ],
 
         'api' => [
-            // 'throttle:60,1',
+            'throttle:60,1',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
             'CORSMiddleware',
             'JsonifyMiddleware',
