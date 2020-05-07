e329ef8cb3728b68e6a9bd370438a4d39fea3457
mohamd-khairy <mohamed.khairy.eg@mail.com>
2020-05-07 18:09:31 +0200

    fix

--- app/Models/User.php
+++ app/Models/User.php
@@ -83,6 +83,15 @@ class User extends VUSER implements JWTSubject
         return $this->hasMany(Topics::class , 'user_id');
     }
 
+    public function experienceDetail()
+    {
+        return $this->hasOne(ExperienceDetails::class , 'user_id')->latest();
+    }
+    
+    public function experienceDetails()
+    {
+        return $this->hasMany(ExperienceDetails::class , 'user_id');
+    }
     /**
      * Get the identifier that will be stored in the subject claim of the JWT.
      *
