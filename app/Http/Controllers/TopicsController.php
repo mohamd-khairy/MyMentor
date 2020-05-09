35c65e0d4b7e6eb3ecdf5ddc5a644dd4304fb5a1
mohamd-khairy <m.khairy@evntoo.com>
2020-05-09 20:21:06 +0200

    fix

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Traits\RestApi;
 use App\Models\Topics;
+use App\Models\User;
 
 class TopicsController extends Controller
 {
@@ -22,10 +23,15 @@ class TopicsController extends Controller
     {
         $searchText = $request->q;
 
+
+        $result = User::whereHas('skills', function ($query) use ($searchText) {
+            $query->where('skill_name', 'like', $searchText);
+        })->get();
+
         // $result = Topics::search($searchText)->paginate(20);
         // return Topics::whereLike(['topic', 'subject', 'category.name', 'language.name'], $searchText)->get();
 
-        $result = Topics::orderby('id' , 'desc')->scopeSearch($searchText)->get();
+        // $result = Topics::orderby('id' , 'desc')->scopeSearch($searchText)->get();
         return \responseSuccess($result);
     }
 }
