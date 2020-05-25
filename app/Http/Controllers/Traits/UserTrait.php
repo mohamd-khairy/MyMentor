2b97c47d86ae2ca11450f550c6b192d4d0c30f3a
mohamd-khairy <m.khairy@evntoo.com>
2020-05-25 19:32:22 +0200

    init

--- app/Http/Controllers/Traits/UserTrait.php
+++ app/Http/Controllers/Traits/UserTrait.php
@@ -26,7 +26,7 @@ trait UserTrait
       $rate = 0 ;
 
       if($data){
-        $rate = $data->sum('rate')/$data->count();
+        $rate = $data->sum('rate');
       }
 
       $user->update(['rate' => $rate]);
