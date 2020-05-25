aa064d79d9a97c2b08d2e8aeb93e9ae7f30133b7
mohamd-khairy <m.khairy@evntoo.com>
2020-05-25 19:52:20 +0200

    init

--- app/Models/Rate.php
+++ app/Models/Rate.php
@@ -45,6 +45,9 @@ class Rate extends Model
 
     public function getCreatedAtAttribute()
     {
-        return $this->attributes['created_at'] ?  date('d M Y' , strtotime($this->attributes['created_at'])) : null;
+        return $this->attributes['created_at'] ?  $this->attributes['created_at'] ->diffForHumans() : null ;
+        // date('d M Y' , strtotime($this->attributes['created_at'])) : null;
+
+       
     }
 }
