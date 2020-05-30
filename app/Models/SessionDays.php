421cb3e1ce3f6bb1a6da0cfcf41535ed600c6294
mohamd-khairy <m.khairy@evntoo.com>
2020-05-30 17:32:59 +0200

    changes

--- app/Models/SessionDays.php
+++ app/Models/SessionDays.php
@@ -9,6 +9,15 @@ class SessionDays extends Model
 {
     protected $fillable= ['date_time' , 'session_id' , 'week_days_id'];
 
+    protected $appends = ['day'];
+
+    public function getDayAttribute()
+    {
+        return  $this->week_days_id? WeekDays::find($this->week_days_id)->day : null;
+    }
+
+
+    /** relations */
     public function session()
     {
         return $this->belongsTo(Sessions::class);
