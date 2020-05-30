3f79918288a2dd9e49de387b061d9a2b7ce7dba9
mohamd-khairy <m.khairy@evntoo.com>
2020-05-30 17:41:13 +0200

    changes

--- app/Models/SessionDays.php
+++ app/Models/SessionDays.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Contracts\Session\Session;
 use Illuminate\Database\Eloquent\Model;
 
@@ -11,6 +12,14 @@ class SessionDays extends Model
 
     protected $appends = ['day'];
 
+    protected $hidden = [ 'created_at' ,'updated_at'];
+
+    protected $dates = [
+        'created_at',
+        'updated_at',
+        'date_time'
+    ];
+
     public function getDayAttribute()
     {
         return  $this->week_days_id? WeekDays::find($this->week_days_id)->day : null;
@@ -19,7 +28,9 @@ class SessionDays extends Model
 
     public function getDateTimeAttribute()
     {
-        return $this->attributes['date_time'] ? 'Started At '.date('d M, Y h:i A' , strtotime($this->attributes['date_time'])) : null;
+        return $this->attributes['date_time'] ?  Carbon::parse($this->attributes['date_time'])->diffForHumans() : null ;
+
+        // return $this->attributes['date_time'] ? 'Started At '.date('d M, Y h:i A' , strtotime($this->attributes['date_time'])) : null;
     }
 
     /** relations */
