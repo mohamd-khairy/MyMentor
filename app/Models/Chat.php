e474b7622e284938d26b22798ba6987381a5a70e
mohamed.khairy <mohamed.khairy@orchtech.com>
2020-06-05 20:06:36 +0200

    changes

--- app/Models/Chat.php
+++ app/Models/Chat.php
@@ -11,18 +11,18 @@ class Chat extends Model
     protected $with = ['messages' , 'user' , 'mentor'];
 
     /** attach loged in user id with chat data */
-    public static function boot()
-    {
-        parent::boot();
+    // public static function boot()
+    // {
+    //     parent::boot();
 
-        static::creating(function ($data) {
-            if(auth('api')->user()->user_type->user_type_name == 'mentor'){
-                $data->mentor_id = auth('api')->user()->id;
-            }else{
-                $data->user_id = auth('api')->user()->id;
-            }
-        });
-    }
+    //     static::creating(function ($data) {
+    //         if(auth('api')->user()->user_type->user_type_name == 'mentor'){
+    //             $data->mentor_id = auth('api')->user()->id;
+    //         }else{
+    //             $data->user_id = auth('api')->user()->id;
+    //         }
+    //     });
+    // }
 
     /** relations */
 
