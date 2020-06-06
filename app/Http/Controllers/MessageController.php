7df91416327dd15200bd9e45c9218009ad098045
mohamed.khairy <mohamed.khairy@orchtech.com>
2020-06-06 15:38:37 +0200

    fix

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -9,7 +9,7 @@ use App\Models\Message;
 class MessageController extends Controller
 {
     const MODEL = Message::class;
-    const FILTERS = [];
+    const FILTERS = ['chat_id'];
 
     use RestApi;
 
