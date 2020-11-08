af51e56e65b2da43757eb2758d571b0a558d0f8f
mohamed.khairy <mohamed.khairy@orchtech.com>
2020-11-09 00:32:01 +0200

    fix

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,9 +7,8 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Support\Facades\Hash;
 use Tymon\JWTAuth\Contracts\JWTSubject;
-use TCG\Voyager\Models\User as VUSER;
 
-class User extends VUSER implements JWTSubject
+class User extends \TCG\Voyager\Models\User implements JWTSubject
 {
     use Notifiable;
 
